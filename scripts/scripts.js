ebe94d5141401dee91c1bbe7040f805d0519e8d1
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2025-01-16 00:29:36 -0600

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const hidden = document.getElementsByClassName(".hidden");
+
+let i = 0;
+
+prev.addEventListener("click", function () {
+  const prevbtn = document.getElementById(`${i}`).classList;
+  prevbtn.remove("selected");
+  prevbtn.add("hidden");
+  if (i === 0) {
+    i = 5;
+  }
+  i--;
+
+  const nextbtn = document.getElementById(`${i}`).classList;
+  nextbtn.remove("hidden");
+  nextbtn.add("selected");
+});
+
+next.addEventListener("click", function () {
+  const prevbtn = document.getElementById(`${i}`).classList;
+  prevbtn.remove("selected");
+  prevbtn.add("hidden");
+  if (i === 4) {
+    i = 0;
+  }
+  i++;
+  const nextbtn = document.getElementById(`${i}`).classList;
+  nextbtn.remove("hidden");
+  nextbtn.add("selected");
+});
