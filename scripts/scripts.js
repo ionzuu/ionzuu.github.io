9234180e856c29dcb1730d8affc46762a101d695
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2025-03-03 23:10:59 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,7 +11,7 @@ prev.addEventListener("click", function () {
   prevbtn.remove("selected");
   prevbtn.add("hidden");
   if (i === 0) {
-    i = 4;
+    i = 6;
   }
   i--;
 
@@ -24,7 +24,7 @@ next.addEventListener("click", function () {
   const prevbtn = document.getElementById(`${i}`).classList;
   prevbtn.remove("selected");
   prevbtn.add("hidden");
-  if (i === 4) {
+  if (i === 6) {
     i = 0;
   }
   i++;
